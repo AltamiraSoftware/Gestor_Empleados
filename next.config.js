eb50bfe47bcd0490ed4e075f24cf9d7e8af48fea
AltamiraSoftware <joakasitto@gmail.com>
2025-02-19 08:44:54 +0100

    Fix: Corregida estructura de la aplicación y rutas

--- next.config.js
+++ next.config.js
@@ -8,6 +8,15 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
+  async redirects() {
+    return [
+      {
+        source: '/empleados',
+        destination: '/',
+        permanent: true,
+      },
+    ]
   }
 }
 
